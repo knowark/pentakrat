47f58ac62082ef568ac24a03d628134629544454
Esteban Echeverry <eecheverry@knowark.com>
2022-01-14 17:39:29 +0000

    Add tests package.

---
+++ app/tests/main.spec.js
@@ -0,0 +1,8 @@
+import { main } from '../src/main'     
+                                                               
+describe('Main', () => {                                       
+  it('defines a main function', () => {                        
+    expect(main).toBeTruthy()                                  
+  })                                                           
+})                                                             
+                                                               
