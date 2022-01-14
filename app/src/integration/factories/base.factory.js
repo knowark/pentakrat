11f7f6cf65790a51b63686f4c1e75069aa605bbb
Esteban Echeverry <eecheverry@knowark.com>
2022-01-14 18:14:16 +0000

    Cover factories.

--- app/src/integration/factories/base.factory.js
+++ app/src/integration/factories/base.factory.js
@@ -3,7 +3,7 @@ import { Routark } from '@knowark/routarkjs'
 
 export class BaseFactory extends Factory {
   router () {
-    return new Routark(window)
+    return new Routark()
   }
 }
 
---
+++ app/tests/integration/factories.spec.js
@@ -0,0 +1,24 @@
+import { Injectark } from '@knowark/injectarkjs'                      
+import { FACTORIES } from '../../src/integration/factories'                                 
+                                                                      
+const FACTORY_TESTS = {                                               
+  base: [                                                             
+    ['Router', 'Routark']                                             
+  ],                                                                  
+}                                                                     
+
+describe('Factories', function () {
+  it('resolve their resources through the injector', function () {
+    for (const [environment, tests] of Object.entries(FACTORY_TESTS)) {
+      const config = {}
+      const factory = FACTORIES[environment](config)
+      const injector = new Injectark({ factory })
+      for (const testCase of tests) {
+        const resource = injector.resolve(testCase[0])
+        expect(resource.constructor.name).toEqual(testCase[1])
+      }
+    }
+  })
+})
+
+
